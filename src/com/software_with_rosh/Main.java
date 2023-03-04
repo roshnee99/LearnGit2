c24cd632f0b1af2f990280043c19b8d9318a62f3
roshnee99 <rs171999@gmail.com>
2023-03-03 17:07:47 -0800

    Fix compilation errors

--- src/com/software_with_rosh/Main.java
+++ src/com/software_with_rosh/Main.java
@@ -1,5 +1,10 @@
-package com.software_with_rosh
+package com.software_with_rosh;
 
-public static void main(String[] args) {
-    System.out.println("This is a cool new main method");
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is a cool new main method");
+    }
 }
